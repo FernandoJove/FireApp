cbf80ecf48f0226644754a18a2ff09fcd45c4c85
unknown <fernandojove@upeu.edu.pe>
2021-10-08 16:32:14 -0500

    ModuloProductos

---
+++ app/src/main/java/com/beta1/androidtest1/ActivityAuth.java
@@ -0,0 +1,16 @@
+package com.beta1.androidtest1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class  ActivityAuth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void 
+}
---
+++ app/src/main/java/com/beta1/androidtest1/Login.java
@@ -0,0 +1,14 @@
+package com.beta1.androidtest1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
